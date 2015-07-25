38e9e3356fd8af8e8faffea801a66f0c9f051d85
gordon <neverlostmine@gmail.com>
2015-07-25 23:54:34 +0800

    Init Commit

---
+++ SimpleManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        /// <summary>
+        /// 員工列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 員工資料暨請假紀錄明細
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 員工資料變更
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Modify()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 請假申請
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LeaveApply()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 本月薪資計算
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CalculateMonthlySalary()
+        {
+            return View();
+        }
+    }
+}
